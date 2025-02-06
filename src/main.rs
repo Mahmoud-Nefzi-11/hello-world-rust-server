6dac4bb47d6b11d9e34f1347f0f17f160b5619ef
Mahmoud Nefzi <mahmoud.nefzi@stockly.ai>
2025-02-06 12:22:53 +0100

    yay

--- src/main.rs
+++ src/main.rs
@@ -21,7 +21,11 @@ fn handle_connection(mut stream: TcpStream) {
         .take_while(|line| !line.is_empty())
         .collect();
 
-    let response = "HTTP/1.1 200 OK\r\n\r\nHello world";
+    let status_line = "HTTP/1.1 200 OK";
+    let contents = "Hello world";
+    let length = contents.len();
+
+    let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
 
     stream.write_all(response.as_bytes()).unwrap();
 }
