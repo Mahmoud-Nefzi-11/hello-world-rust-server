3d910b1e27774ef0171b7ed63a62e2d4a381e9b3
Mahmoud Nefzi <mahmoud.nefzi@stockly.ai>
2025-02-11 12:04:59 +0100

    fixed response headers

--- src/main.rs
+++ src/main.rs
@@ -21,11 +21,17 @@ fn handle_connection(mut stream: TcpStream) {
         .take_while(|line| !line.is_empty())
         .collect();
 
+    // Respond with a simple "Hello world"
     let status_line = "HTTP/1.1 200 OK";
     let contents = "Hello world";
     let length = contents.len();
+    let content_type = "Content-Type: text/plain; charset=UTF-8";
 
-    let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+    // Format the HTTP response
+    let response = format!(
+        "{status_line}\r\n{content_type}\r\nContent-Length: {length}\r\n\r\n{contents}"
+    );
 
+    // Send the response to the client
     stream.write_all(response.as_bytes()).unwrap();
 }
